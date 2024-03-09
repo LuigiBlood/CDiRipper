7570a3c9f7e0b8848119af5365ee33ba6695b295
LuigiBlood <luigiblood@gmail.com>
2024-03-09 01:57:57 +0100

    Add VS Project

---
+++ CDiRipper/FormMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace CDiRipper
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.listBoxIndex = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load...";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxImage.Location = new System.Drawing.Point(142, 48);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(384, 280);
+            this.pictureBoxImage.TabIndex = 1;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // listBoxIndex
+            // 
+            this.listBoxIndex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxIndex.FormattingEnabled = true;
+            this.listBoxIndex.Location = new System.Drawing.Point(12, 51);
+            this.listBoxIndex.Name = "listBoxIndex";
+            this.listBoxIndex.Size = new System.Drawing.Size(120, 277);
+            this.listBoxIndex.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 340);
+            this.Controls.Add(this.listBoxIndex);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.buttonLoad);
+            this.Name = "FormMain";
+            this.Text = "CD-i Ripper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.ListBox listBoxIndex;
+    }
+}
+
